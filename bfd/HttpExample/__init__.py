d2d6fd4082ede3344b5aa77dde57711f33161bfc
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2020-10-15 20:57:24 +0200

    readme

--- bfd/HttpExample/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-import azure.functions as func
-
-def main(documents: func.DocumentList) -> str:
-    if documents:
-        logging.info('First document Id modified: %s', documents[0]['id'])
